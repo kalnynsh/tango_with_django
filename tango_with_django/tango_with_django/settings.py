365334ebea5f1ce09bc4771338ff6c8cd5c1c7be
kalnynsh <kda869@yandex.ru>
2016-12-27 15:09:31 +0300

    13. In /tango_with_django/settings.py added MEDIA_DIR, MEDIA_ROOT, MEDIA_URL for serving madia files. Also added in TEMPLATE 'context_processors': [..., 'django.template.context_processors.media'].

--- tango_with_django/tango_with_django/settings.py
+++ tango_with_django/tango_with_django/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,3 +126,11 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+
+# Media files
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
