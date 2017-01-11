835e108f39ad4ef46129f8109379882cd0e0b386
kalnynsh <kda869@yandex.ru>
2017-01-11 15:44:32 +0300

    23. In ~/populate_rango.py revise code, add views to page.

--- tango_with_django/populate_rango.py
+++ tango_with_django/populate_rango.py
@@ -16,34 +16,48 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url": "http://docs.python.org/3/tutorial/"},
+         "url": "http://docs.python.org/3/tutorial/",
+         "views": 32},
         {"title": "How to Think like a Computer Scientist",
-         "url": "http://www.greenteapress.com/thinkpython/"},
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 16},
         {"title": "Learn Python in 10 Minutes",
-         "url": "https://www.stavros.io/tutorials/python/"},
+         "url": "https://www.stavros.io/tutorials/python/",
+         "views": 8},
     ]
 
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.10/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.10/intro/tutorial01/",
+         "views": 32},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/",
+         "views": 16},
         {"title": "How tango with Django",
-         "url": "http://www.tangowithdjango.com/"},
+         "url": "http://www.tangowithdjango.com/",
+         "views": 8},
         {"title": "Django book",
-         "url": "http://djbook.ru/"},
+         "url": "http://djbook.ru/",
+         "views": 10},
     ]
 
     other_pages = [
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 32},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org"},
+         "url": "http://flask.pocoo.org",
+         "views": 16},
     ]
 
-    cats = {"Python": [{"views": 128, "likes": 64}, {"pages": python_pages}, ],
-            "Django": [{"views": 64, "likes": 32}, {"pages": django_pages}, ],
-            "Other Frameworks": [{"views": 32, "likes": 16}, {"pages": other_pages}, ],
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Pascal": {"pages": [], "views": 32, "likes": 16},
+            "Perl": {"pages": [], "views": 32, "likes": 16},
+            "Php": {"pages": [], "views": 32, "likes": 16},
+            "Prolog": {"pages": [], "views": 32, "likes": 16},
+            "Programming": {"pages": [], "views": 32, "likes": 16},
             }
 
 # If you want to add more catergories or pages,
@@ -55,12 +69,9 @@ def populate():
 # for more information about how to iterate over a dictionary properly.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
-        c.views = cat_data[0]["views"]
-        c.likes = cat_data[0]["likes"]
-        c.save()
-        for p in cat_data[1]["pages"]:
-            add_page(c, p["title"], p["url"])
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -77,7 +88,9 @@ def add_page(cat, title, url, views=0):
 
 
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
