4dcba902d469b9d4dd0893f350d474cb62aed2b7
kalnynsh <kda869@yandex.ru>
2017-01-12 15:59:33 +0300

    24. In rango/views.py from rango.models import Category, Page. Edite index() by add category_list queryset and return in render().

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rango.models import Category, Page
 
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for list of ALL categories currently stored.
+    # Order the categories by number likes in descending order.
+    # Retrieve the top only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
