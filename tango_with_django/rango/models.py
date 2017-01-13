eda1ad279a8e8f48167d1e16cd0996721f574193
kalnynsh <kda869@yandex.ru>
2017-01-13 17:47:21 +0300

    35. In /rango/models.py  replace IntegerField to PositiveIntegerField.

--- tango_with_django/rango/models.py
+++ tango_with_django/rango/models.py
@@ -4,8 +4,8 @@ from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True)  # blank=True
 
     def save(self, *args, **kwargs):
