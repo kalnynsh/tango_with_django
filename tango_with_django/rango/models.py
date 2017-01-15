08d97b3c7d1c2654f767e90205706bfe6ada8f7e
kalnynsh <kda869@yandex.ru>
2017-01-15 10:24:36 +0300

    42. In rango/models.py add consts MAX_LENGTH_200(128).

--- tango_with_django/rango/models.py
+++ tango_with_django/rango/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+MAX_LENGTH_128 = 128
+MAX_LENGTH_200 = 200
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=MAX_LENGTH_128, unique=True)
     views = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True)  # blank=True
@@ -21,8 +24,8 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=MAX_LENGTH_128)
+    url = models.URLField(max_length=MAX_LENGTH_200)
     views = models.IntegerField(default=0)
 
     def __str__(self):
