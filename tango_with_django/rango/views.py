592471db54752eb9e00c3a7dcb3cdbdb45064aa3
kalnynsh <kda869@yandex.ru>
2017-02-02 18:09:18 +0300

    68. In /rango/views add def user_logout() with decorator login_required. Add import statement for logout.

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
@@ -230,3 +230,12 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use the login_require() decorator to ensure only those logged in can access the view
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log out.
+    logout(request)
+    # Take the user back to the homepage.
+    return HttpResponseRedirect(reverse('index'))
