c814a6c80bb19908cb0182055bdd8a604233efd7
kalnynsh <kda869@yandex.ru>
2017-02-02 18:12:02 +0300

    69. In /rango/urls add url(r'^logout/$', views.user_logout, ...).

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
         name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
