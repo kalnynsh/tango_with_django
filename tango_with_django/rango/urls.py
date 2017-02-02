539dfc58545737c000ed5b0eebe767853b118a06
kalnynsh <kda869@yandex.ru>
2017-02-02 17:42:12 +0300

    66. In /rango/urls add url(r'^restricted/', views.restricted, ...).

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^register/$', views.register,
         name='register'),
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^restricted/', views.restricted, name='restricted'),
 ]
