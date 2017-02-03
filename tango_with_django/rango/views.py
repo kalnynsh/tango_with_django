8af166c448db0a143a18386544368ee928069ea6
kalnynsh <kda869@yandex.ru>
2017-02-03 18:17:21 +0300

    72. In /rango/views modify restricted() using created /templates/rango/restricted.html.

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -231,7 +231,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 
 # Use the login_require() decorator to ensure only those logged in can access the view
