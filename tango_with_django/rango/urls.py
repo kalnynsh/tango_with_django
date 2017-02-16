1ca9c8e365ac4e14d7f5d26d2c7dc5dbf628812e
kalnynsh <kda869@yandex.ru>
2017-02-16 17:48:29 +0300

    99. In /rango/urls.py add url(r'search/$', ...).

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
     # url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^restricted/$', views.restricted, name='restricted'),
+    url(r'search/$', views.search, name='search'),
 ]
