4f241a02ca047f33bd09416a7bc91cb55f643404
kalnynsh <kda869@yandex.ru>
2017-02-21 14:20:41 +0300

    105. In /templates/rango/category.html add Search form. In /rango/views.py refactor category() displaying results.

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -57,28 +57,38 @@ def show_category(request, category_name_slug):
         # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
-
         category = Category.objects.get(slug=category_name_slug)
 
         # Retrieve all of the associated pages.
         # Note that filter() will return a list of page objects or an empty list.
-
         pages = Page.objects.filter(category=category)
 
         # Adds our results list to the template context under name pages.
-
         context_dict['pages'] = pages
         # We also add the category object from
         # the database to the context dictionary.
         # We'll use this in the template to verify that the category exists.
 
         context_dict['category'] = category
+        # Create a default query based on the category name
+        # to be shown in the search box
+        context_dict['query'] = category.name
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything -
         # the template will display the "no category" message for us.
         context_dict['category'] = None
         context_dict['pages'] = None
+
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            # Run our Webhose (Bing) function to get the results list!
+            result_list = run_query_bing(query)
+            # result_list = run_query_webhose(query)
+            context_dict['query'] = query
+    context_dict['result_list'] = result_list
 
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
