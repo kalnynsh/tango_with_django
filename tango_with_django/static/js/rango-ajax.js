6ead564395a70fc3c3c7d222688671f241e38d70
kalnynsh <kda869@yandex.ru>
2017-02-25 17:59:06 +0300

    126. In /static/js/rango-ajax.js add function handler like_count.

--- tango_with_django/static/js/rango-ajax.js
+++ tango_with_django/static/js/rango-ajax.js
@@ -1,3 +1,10 @@
 $(function() {
-
+    $('#likes').click(function() {
+        var catid;
+        catid = $(this).attr("data-catid");
+        $.get('/rango/like/', {category_id: catid}, function(data) {
+            $('#like_count').html(data);
+            $('#likes').hide();
+        });
+    });
 });
