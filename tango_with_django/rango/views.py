18420abe3f3399e12df549ac8af2e953bd59999d
kalnynsh <kda869@yandex.ru>
2017-02-25 12:37:18 +0300

    123. In /rango/views change position to head get_server_side_cookie and visitor_cookie.

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -16,6 +16,38 @@ from django.contrib.auth.models import User
 # from django.core.urlresolvers import reverse
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+# Update the function definition
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:  # (datetime.now() - last_visit_time).seconds
+        visits += 1
+        # update the last visit cookie now that we have updated the count
+        request.sesion['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -152,6 +184,19 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 
+def search(request):
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        # query_placeholder = query
+        if query:
+            # Run our Bing (90 d free) or Webhose (free) function to get the results list!
+            result_list = run_query_bing(query)
+            # result_list = run_query_webhose(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list, })
+
+
 # Using - registration.backends.simple.urls
 def register(request):
     # A boolean value for telling the template
@@ -273,50 +318,6 @@ def restricted(request):
 
 
 # A helper method
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-# Update the function definition
-def visitor_cookie_handler(request):
-    # Get the number of visits to the site.
-    # We use the COOKIES.get() function to obtain the visits cookie.
-    # If the cookie exists, the value returned is casted to an integer.
-    # If the cookie doesn't exist, then the default value of 1 is used.
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        '%Y-%m-%d %H:%M:%S')
-
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:  # (datetime.now() - last_visit_time).seconds
-        visits += 1
-        # update the last visit cookie now that we have updated the count
-        request.sesion['last_visit'] = str(datetime.now())
-    else:
-        visits = 1
-        # set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie
-
-    # Update/set the visits cookie
-    request.session['visits'] = visits
-
-
-def search(request):
-    result_list = []
-    if request.method == 'POST':
-        query = request.POST['query'].strip()
-        # query_placeholder = query
-        if query:
-            # Run our Bing (90 d free) or Webhose (free) function to get the results list!
-            result_list = run_query_bing(query)
-            # result_list = run_query_webhose(query)
-
-    return render(request, 'rango/search.html', {'result_list': result_list, })
 
 
 def track_url(request):
