7b6b8d0920742d9206509e2e07518980674769e6
kalnynsh <kda869@yandex.ru>
2017-02-25 20:13:45 +0300

    127. In /static/js/rango-ajax.js revise $.get('/rango/like_category/'..). In /rango/urls edit r'like/$'. In /rango/views refactor code.

--- tango_with_django/static/js/rango-ajax.js
+++ tango_with_django/static/js/rango-ajax.js
@@ -2,7 +2,7 @@ $(function() {
     $('#likes').click(function() {
         var catid;
         catid = $(this).attr("data-catid");
-        $.get('/rango/like/', {category_id: catid}, function(data) {
+        $.get('/rango/like_category/', { category_id: catid }, function(data) {
             $('#like_count').html(data);
             $('#likes').hide();
         });
