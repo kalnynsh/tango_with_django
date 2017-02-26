314a405e48277043066d8359ba74b5ef93510fad
kalnynsh <kda869@yandex.ru>
2017-02-26 18:12:52 +0300

    133. In /js/rango-ajax add functionality for add page to category.

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -5,7 +5,7 @@ from rango import views
 app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
+    url(r'about/', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
@@ -19,8 +19,8 @@ urlpatterns = [
     url(r'search/$', views.search, name='search'),
     url(r'goto/$', views.track_url, name='goto'),
     url(r'like/$', views.like_category, name='like_category'),
-    url(r'suggest/$', views.suggest_category, name='suggest_category'),
-    url(r'register_profile/$', views.register_profile, name='register_profile'),
-    url(r'profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
-    url(r'profiles/$', views.list_profiles, name='list_profiles'),
+    url(r'^suggest/$', views.suggest_category, name='suggest_category'),
+    url(r'^register_profile/$', views.register_profile, name='register_profile'),
+    url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
+    url(r'^profiles/$', views.list_profiles, name='list_profiles'),
 ]
