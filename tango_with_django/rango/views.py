bf6e1bc3cb38266f41dc573b71265f5fb60d85b3
kalnynsh <kda869@yandex.ru>
2017-02-26 13:09:36 +0300

    131. In /rango/view add def suggest_category(request) return render to rango/cats.html with category list.

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -415,3 +415,14 @@ def get_category_list(max_results=0, starts_with=''):
                 cat_list = cat_list[:max_results]
 
     return cat_list
+
+
+def suggest_category(request):
+    cat_list = []
+    starts_with = ''
+
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+        cat_list = get_category_list(8, starts_with)
+
+    return render(request, 'rango/cats.html', {'cats': cat_list, })
