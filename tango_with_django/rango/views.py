f4bb17522cc84e6910204e4c1f979be4cf4ac045
kalnynsh <kda869@yandex.ru>
2017-02-26 13:02:38 +0300

    130. In /rango/view add def get_category_list(max_results=0, starts_with='')...

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -404,3 +404,14 @@ def like_category(request):
             cat.save()
     return HttpResponse(likes)
 
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = Category.objects.filter(name__istartswith=starts_with)
+
+        if max_results > 0:
+            if len(cat_list) > max_results:
+                cat_list = cat_list[:max_results]
+
+    return cat_list
